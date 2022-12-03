de20bb8d5136f818698a7722d1aed6d18a4f1188
Tom McLean <tom.mclean@bartechnologies.uk>
2022-12-03 18:45:22 +0000

    part 2

--- 02/main.cpp
+++ 02/main.cpp
@@ -12,12 +12,17 @@ enum RockPaperSissors {
     Sissors
 };
 
-typedef std::tuple<RockPaperSissors, RockPaperSissors> game;
+enum GameResult {
+    Loss,
+    Draw,
+    Win
+};
 
 int main() {
-    int lossPoints = 0;
-    int drawPoints = 3;
-    int winPoints = 6;
+    std::unordered_map<GameResult, int> resultToPoints;
+    resultToPoints[Loss] = 0;
+    resultToPoints[Draw] = 3;
+    resultToPoints[Win] = 6;
 
     std::unordered_map<RockPaperSissors, int> playerPoints = {
         {Rock, 1},
@@ -28,19 +33,37 @@ int main() {
     std::unordered_map<RockPaperSissors, std::unordered_map<RockPaperSissors, int>> gameMap;
     
     gameMap[Rock] = {
-        {Rock, drawPoints},
-        {Paper, lossPoints},
-        {Sissors, winPoints}
+        {Rock, Draw},
+        {Paper, Loss},
+        {Sissors, Win}
     };
     gameMap[Paper] = {
-        {Rock, winPoints},
-        {Paper, drawPoints},
-        {Sissors, lossPoints}
+        {Rock, Win},
+        {Paper, Draw},
+        {Sissors, Loss}
     };
     gameMap[Sissors] = {
-        {Rock, lossPoints},
-        {Paper, winPoints},
-        {Sissors, drawPoints}
+        {Rock, Loss},
+        {Paper, Win},
+        {Sissors, Loss}
+    };
+
+    std::unordered_map<RockPaperSissors, std::unordered_map<GameResult, RockPaperSissors>> inverseGameMap;
+
+    inverseGameMap[Rock] = {
+        {Loss, Sissors},
+        {Draw, Rock},
+        {Win, Paper}
+    };
+    inverseGameMap[Paper] = {
+        {Loss, Rock},
+        {Draw, Paper},
+        {Win, Sissors}
+    };
+    inverseGameMap[Sissors] = {
+        {Loss, Paper},
+        {Draw, Sissors},
+        {Win, Rock}
     };
 
     std::unordered_map<char, RockPaperSissors> cToEnumMap = {
@@ -52,14 +75,20 @@ int main() {
         {'Z', Sissors}
     };
 
+    std::unordered_map<char, GameResult> cToGameResult = {
+        {'X', Loss},
+        {'Y', Draw},
+        {'Z', Win}
+    };
 
     std::ifstream infile("input.txt");
     std::string line;
     size_t totalScore = 0;
     for (std::string line; std::getline(infile, line);) {
         RockPaperSissors opponent = cToEnumMap[line[0]];
-        RockPaperSissors me = cToEnumMap[line[2]];
-        totalScore += gameMap[me][opponent];
+        GameResult result = cToGameResult[line[2]];
+        RockPaperSissors me = inverseGameMap[opponent][result];
+        totalScore += resultToPoints[result];
         totalScore += playerPoints[me];
     };
 
